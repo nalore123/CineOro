4e8a91933c1b26f13a02bb514561e4333e7a65aa
nalore123 <kosiclorena@gmail.com>
2026-01-28 19:24:34 +0100

    dodala sam modele i relacije

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+model_list = [Genre, Movie, Review]
+admin.site.register(model_list)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+#uvodi se ugradeni user model iz django autentifikacijskog sustava kako 
+# bi se recenzije povezale s korisnicima bez kreiranja user modela
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class Genre(models.Model):#zanrovi
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):#filmovi
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    release_year = models.IntegerField()
+    genre = models.ForeignKey(Genre, default=1, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):#model za recenziju
+    movie = models.ForeignKey(Movie, default=1, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.movie.title}"
