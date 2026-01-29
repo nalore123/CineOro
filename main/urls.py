45704f3eca272b6a35a4c538ba8c8ead93bb14f8
nalore123 <kosiclorena@gmail.com>
2026-01-29 22:45:13 +0100

    dodala sam genericke poglede i base_generic

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from main.views import *
 
 app_name = 'main' 
 
 urlpatterns = [
+    path('', GenreList.as_view()),
+    path('movies/', MovieList.as_view()),
+    path('<str:genre>/', GenreFilmList.as_view()),
+    path('<str:genre>/<str:movie>/', MovieReviewList.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from main.models import *
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+class MovieList(ListView):
+    model = Movie
+    template_name='main/movies_by_genre.html'
+
+class GenreList(ListView):
+    model = Genre
+
+class GenreFilmList(ListView):
+    template_name = 'main/movies_by_genre.html'
+
+    def get_queryset(self):
+        self.genre = get_object_or_404(Genre, name=self.kwargs['genre'])
+        return Movie.objects.filter(genre=self.genre)
+    
+class MovieReviewList(ListView):
+    template_name = 'main/reviews_by_movie.html'
+
+    def get_queryset(self):
+        self.movie = get_object_or_404(Movie, title=self.kwargs['movie'])
+        return Review.objects.filter(movie=self.movie)
